1c182939755af2f7d82ec38da57428b5a5bbe8da
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-11-23 23:59:33 -0300

    remove configuração de exclusão do ícone BookMark no Rollup

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  build: {
-    rollupOptions: {
-      external: ['vue-material-design-icons/BookMark.vue'],
-    },
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
